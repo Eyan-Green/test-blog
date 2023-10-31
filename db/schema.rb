c4788e34e0c8a980f05509bf96a5a2c711acc1ed
Ian Green <ian.green@ics-digital.com>
2023-10-31 16:36:54 +0000

    Add provider to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_31_155913) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_31_163604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_31_155913) do
     t.string "full_name"
     t.integer "user_type_id"
     t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_type_id"], name: "index_users_on_user_type_id"
